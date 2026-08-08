369db5447bca2b71c38270162cb6935c6b1a72f3
prem0592 <premkumar0592@gmail.com>
2026-08-09 00:20:42 +0530

    Update cache name in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="pulse-commercial-v10-google";
+const CACHE="pulse-commercial-v10-google-greeting-v1";
 const CORE=["./","./index.html","./manifest.json","./config.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
