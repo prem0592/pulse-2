8a25405988114a66ceb9642ebd1ffbd4803c8269
prem0592 <premkumar0592@gmail.com>
2026-08-08 20:27:42 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="pulse-shell-v3";const ASSETS=["./","./index.html","./manifest.json","./config.js"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match("./index.html"))));});
